cc5e5dbcd2f7c025a433bce9a345829ce3c70989
Akshaj Jagarapu <askjag07@gmail.com>
2021-05-06 16:44:58 +0530

    Improved environment variable handling.

--- server/router.go
+++ server/router.go
@@ -1,6 +1,9 @@
 package server
 
 import (
+	"log"
+	"strconv"
+
 	"github.com/gin-gonic/gin"
 	"go.mongodb.org/mongo-driver/mongo"
 
@@ -12,7 +15,13 @@ import (
 func NewRouter(client *mongo.Client) *gin.Engine {
 
 	Production := config.GetVar("PRODUCTION")
-	if Production == "true" {
+
+	production, err := strconv.ParseBool(Production)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	if production {
 		gin.SetMode(gin.ReleaseMode)
 	}
 
